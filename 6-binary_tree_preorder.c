5845413f62b00fb96905749f652ac0f380044965
Julius M. Kanani <juliusm.kanani@gmail.com>
2024-03-01 22:10:45 +0300

    trial and error to fix a bug

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -11,10 +11,10 @@
   */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree != NULL)
-	{
-		func(tree->n);
-		binary_tree_preorder(tree->left, func);
-		binary_tree_preorder(tree->right, func);
-	}
+	if (!(tree) || !(func))
+		return;
+
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
